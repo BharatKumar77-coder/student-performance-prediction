ac7e535981aa17418b2c1eaa8d782828af2d2fab
BHARAT KUMAR <bharatkumarch1122@gmail.com>
2025-06-19 19:12:40 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,6 +4,10 @@ from src.exception import CustomException
 from src.logger import logging
 from src.components.data_transformation import DataTranformation
 from src.components.data_transformation import DataTranformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
+
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
@@ -51,5 +55,8 @@ if __name__ == "__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation = DataTranformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
             
